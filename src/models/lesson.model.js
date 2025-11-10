0f78610590c127831320cf7b47c4733aff8c9db3
nguyen.dk <nd2004lk13@gmail.com>
2025-11-10 18:14:58 +0700

    refactor: add max length 50 for title lesson to prevent layout issues

--- src/models/lesson.model.js
+++ src/models/lesson.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const lessonSchema = new mongoose.Schema({
-    title: { type: String, required: true, trim: true },
+    title: { type: String, maxlength: 50, required: true, trim: true },
     path: { type: String, default: "" }, // path HTML
     type: { type: String, enum: ["reading", "vocabulary"], required: true },
     views: { type: Number, default: 0 },
