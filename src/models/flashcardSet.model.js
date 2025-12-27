84dda4c514d97339570770bb14bbbc3af172fe6b
NhatNguyen1784 <nguyennhatnguyen1782004@gmail.com>
2025-12-27 15:36:39 +0700

    refactor: add maxlength validation to flashcard and flashcardSet schemas

--- src/models/flashcardSet.model.js
+++ src/models/flashcardSet.model.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const flashcardSetSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    name: { type: String, required: true },
+    name: { type: String, required: true, maxlength: 50 },
     count: { type: Number, default: 0 },
-    description: { type: String },
+    description: { type: String, maxlength: 100 },
 }, { timestamps: true });
 
 export default mongoose.model('FlashcardSet', flashcardSetSchema);
